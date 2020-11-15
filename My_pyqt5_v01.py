c45eae5de827ce9a685f10f4433d15fb56cfd128
rpdacosta <masterminded_dacosta@hotmail.com>
2020-11-15 01:48:39 +0100

    MediaInfo working

--- My_pyqt5_v01.py
+++ My_pyqt5_v01.py
@@ -6,6 +6,9 @@ from PyQt5 import QtCore as qtc
 from PyQt5 import QtGui as qtg
 import qtmodern.styles
 import qtmodern.windows
+from ffprobe import FFProbe
+from pprint import pprint
+from pymediainfo import MediaInfo
 
 
 class Mainwindow(qtw.QMainWindow):
@@ -35,6 +38,32 @@ class Mainwindow(qtw.QMainWindow):
          'c:\\',"Video Files (*.mp4 *.flv *.ts *.mts *.avi)")
         videoname_url = str(fname[0])
         self.ui.label_videoname.setText(videoname_url)
+
+        data_list =[]
+        media_info = MediaInfo.parse(videoname_url)
+        for track in media_info.tracks:
+            
+            if track.track_type == "Video":
+                print("Bit rate: {t.bit_rate}, Frame rate: {t.frame_rate}, "
+                    "Format: {t.format}".format(t=track)
+                )
+                print("Duration (raw value):", track.duration)
+                #print("Duration (other values:")
+                pprint(track.other_duration)
+                self.ui.textEdit_data.append( "Bit rate: {t.bit_rate}, Frame rate: {t.frame_rate}, "
+                    "Format: {t.format}".format(t=track))
+                self.ui.textEdit_data.append("Duration (raw value):" +  str(track.duration) + '\n')
+                self.ui.textEdit_data.append("Duration (other values:")
+                #self.ui.textEdit_data.setText(self.ui.textEdit_data.text() + str(track ))
+                
+
+            elif track.track_type == "Audio":
+                print("Track data:")
+                pprint(track.to_data())
+                self.ui.textEdit_data.append("Track data:" + str(track.to_data()))
+
+
+
 
 
 
