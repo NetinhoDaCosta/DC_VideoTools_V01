dc1d69770bd3c0f95f0d35f681148eee0df2b3b2
NetinhoDaCosta <rpdacostaneto@gmail.com>
2020-12-01 11:50:31 +0100

    new data

--- My_pyqt5_v02.py
+++ My_pyqt5_v02.py
@@ -22,7 +22,7 @@ class Mainwindow(qtw.QMainWindow):
 
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.ui.select_btn.clicked.connect(self.loop_video)
+        self.ui.select_btn.clicked.connect(self.show_data)
         self.ui.audio_btn.clicked.connect(self.cut_audio)
 
         # my ui code ends ere
@@ -70,7 +70,69 @@ class Mainwindow(qtw.QMainWindow):
     def exact_size(self):
         pass
 
-    def loop_video(self):
+    def show_data(self):
+        print("ok")
+        fname = qtw.QFileDialog.getOpenFileName(self, 'Open file',
+         'c:\\',"Video Files (*.mp4 *.flv *.ts *.mts *.avi)")
+        videoname_url = str(fname[0])
+        self.ui.label_videoname.setText(videoname_url)
+
+        data_list =[]
+
+        media_info = MediaInfo.parse(videoname_url)
+        for track in media_info.tracks:
+
+            if track.track_type == "Video":
+                print("Bit rate: {t.bit_rate}, Frame rate: {t.frame_rate}, "
+                    "Format: {t.format}".format(t=track)
+                )
+                print("Duration (raw value):", track.duration)
+                #print("Duration (other values:")
+                pprint(track.other_duration)
+                self.ui.textEdit_data.append( "Bit rate: {t.bit_rate}, Frame rate: {t.frame_rate}, Format: {t.format}".format(t=track))
+                self.ui.textEdit_data.append("Duration (raw value):" +  str(track.duration) + '\n')
+                self.ui.textEdit_data.append(str(track.other_duration))
+                #self.ui.textEdit_data.setText(self.ui.textEdit_data.text() + str(track ))
+
+
+            elif track.track_type == "Audio":
+                print("Track audio-data:")
+                pprint(track.to_data())
+                data_dict = track.to_data()
+                for key, val in data_dict.items():
+                  print(key)
+                  self.ui.textEdit_data.append("Track audio-data:" + str(key) + " : " + str(val))
+
+
+        in_file = ffmpeg.input(videoname_url)
+        probe = ffmpeg.probe(videoname_url)
+        video_stream = next((stream for stream in probe['streams'] if stream['codec_type'] == 'video'), None)
+        video_stream2 = next((stream for stream in probe['streams']), None)
+        # width = int(video_stream['width'])
+        # height = int(video_stream['height'])
+        print(type(video_stream2))
+
+        for key, value in video_stream2.items():
+            print(key, " : ", value )
+            file_data = str(key) +  " : " + str(value)
+            
+            self.ui.textEdit_data.append(str(file_data))
+        
+        # for stream_item in enumerate(video_stream2):
+        #     print(video_stream2[stream_item])
+
+        for my_data_x in probe['streams']:
+            pass
+            # print("*******************************************************")
+            # print(my_data_x)
+
+            # for my_data_y in probe[my_data_x]:
+            #     print(   my_data_y,":", probe[my_data_x][my_data_y])
+            
+
+
+
+    def show_data_backup(self):
         print("ok")
         fname = qtw.QFileDialog.getOpenFileName(self, 'Open file',
          'c:\\',"Video Files (*.mp4 *.flv *.ts *.mts *.avi)")
@@ -102,6 +164,9 @@ class Mainwindow(qtw.QMainWindow):
                 for key, val in data_dict.items():
                   print(key)
                   self.ui.textEdit_data.append("Track data:" + str(key) + " : " + str(val))
+
+    def loop_video(self):
+        pass
 
 
 
